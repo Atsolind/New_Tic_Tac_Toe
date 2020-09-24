a0b205230805e1583c75aab0cf4681095f9a5cbb
Atsolind <61784526+Atsolind@users.noreply.github.com>
2020-09-24 22:04:06 +0300

    working game

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import "./styles.css";
 
 let currentTurn = "O";
+let gameActive = true;
 
 function createTable() {
   const table = document.getElementById("board");
@@ -28,6 +29,7 @@ function insertValue(tableCell, table) {
     tableCell.innerHTML = "O";
     changePlayer();
   }
+  checkWinner();
 }
 
 function cellClick(table) {
@@ -40,4 +42,98 @@ function cellClick(table) {
   }
 }
 
-function tableInserts(tableCell, table) {}
+const winCombinations = [
+  [0, 1, 2, 3, 4],
+  [5, 6, 7, 8, 9],
+  [10, 11, 12, 13, 14],
+  [15, 16, 17, 18, 19],
+  [20, 21, 22, 23, 24],
+  [0, 5, 10, 15, 20],
+  [1, 6, 11, 16, 21],
+  [2, 7, 12, 17, 22],
+  [3, 8, 13, 18, 23],
+  [4, 9, 14, 19, 24],
+  [0, 6, 12, 18, 24],
+  [4, 8, 12, 16, 20]
+];
+
+function checkWinner() {
+  let roundWin = false;
+  let table = document.getElementById("board");
+  for (let i = 0; i <= 4; i++) {
+    let a = table.rows[i].cells[0].innerHTML;
+    let b = table.rows[i].cells[1].innerHTML;
+    let c = table.rows[i].cells[2].innerHTML;
+    let d = table.rows[i].cells[3].innerHTML;
+    let e = table.rows[i].cells[4].innerHTML;
+
+    if (a === "" || b === "" || c === "" || d === "" || e === "") {
+      continue;
+    }
+    if (a === b && b === c && c === d && d === e) {
+      roundWin = true;
+      break;
+    }
+  }
+  for (let i = 0; i <= 4; i++) {
+    let a = table.rows[0].cells[i].innerHTML;
+    let b = table.rows[1].cells[i].innerHTML;
+    let c = table.rows[2].cells[i].innerHTML;
+    let d = table.rows[3].cells[i].innerHTML;
+    let e = table.rows[4].cells[i].innerHTML;
+
+    if (a === "" || b === "" || c === "" || d === "" || e === "") {
+      continue;
+    }
+    if (a === b && b === c && c === d && d === e) {
+      roundWin = true;
+      break;
+    }
+  }
+  for (let i = 0; i <= 4; i++) {
+    let a = table.rows[0].cells[0].innerHTML;
+    let b = table.rows[1].cells[1].innerHTML;
+    let c = table.rows[2].cells[2].innerHTML;
+    let d = table.rows[3].cells[3].innerHTML;
+    let e = table.rows[4].cells[4].innerHTML;
+
+    if (a === "" || b === "" || c === "" || d === "" || e === "") {
+      continue;
+    }
+    if (a === b && b === c && c === d && d === e) {
+      roundWin = true;
+      break;
+    }
+  }
+  for (let i = 0; i <= 4; i++) {
+    let a = table.rows[0].cells[4].innerHTML;
+    let b = table.rows[1].cells[3].innerHTML;
+    let c = table.rows[2].cells[2].innerHTML;
+    let d = table.rows[3].cells[1].innerHTML;
+    let e = table.rows[4].cells[0].innerHTML;
+
+    if (a === "" || b === "" || c === "" || d === "" || e === "") {
+      continue;
+    }
+    if (a === b && b === c && c === d && d === e) {
+      roundWin = true;
+      break;
+    }
+  }
+  if (roundWin) {
+    if (currentTurn === "X") {
+      alert("Player 1 won!");
+    }
+    if (currentTurn === "O") {
+      alert("Player 2 won!");
+    }
+    gameActive = false;
+    resetTable();
+    return;
+  }
+}
+
+function resetTable() {
+  document.getElementById("board").innerHTML = "";
+  createTable();
+}
