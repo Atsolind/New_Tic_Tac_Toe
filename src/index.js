a40cb325f6ea5a9d525a8979b306c95ddd32cbc6
Atsolind <61784526+Atsolind@users.noreply.github.com>
2020-10-03 14:58:51 +0300

    Finished progressbar.

--- src/index.js
+++ src/index.js
@@ -1,60 +1,28 @@
 import "./styles.css";
-
 let currentTurn = "O";
-let gameActive = true;
-var turn = document.getElementById("insidebar");
-var turnCounter = 0;
-var width = 0;
-var id;
-var seconds = 0;
-var time = document.getElementById("stopwatch");
-var timeInterval;
-var timeout;
-
-function timeOut() {
-  turnCounter++;
-  alert("Time has run out, other players turn.");
-  changePlayer();
-}
-
-function resetTimeout() {
-  clearTimeout(timeout);
-}
-function rewindTimer() {
-  time.innerHTML = 10 - seconds;
-  seconds++;
-}
-if ((turnCounter = 0)) {
-  timeInterval = setInterval(rewindTimer, 1000);
-}
+var time = document.getElementById("insidebar");
+let width = 0;
+let id;
 
 function movebar() {
   id = setInterval(frame, 100);
   function frame() {
     if (width >= 100) {
+      changePlayer();
       resetWidth();
+      alert("Too slow, changin player.");
     } else {
       width++;
-      turn.style.width = width + "%";
-      turn.innerHTML = Math.round(width * 0.1) + " Sec";
+      time.style.width = width + "%";
+      time.innerHTML = Math.round(width * 0.1) + " Sec";
     }
-  }
-}
-
-function move() {
-  if (width >= 25) {
-    resetWidth();
-  } else {
-    width = width + 1;
-    turn.style.width = width + "%";
-    turn.innerHTML = width + ".Turn";
   }
 }
 
 function resetWidth() {
   width = 0;
   clearInterval(id);
-  turn.style.width = width + "%";
+  time.style.width = width + "%";
 }
 
 function createTable() {
@@ -67,38 +35,23 @@ function createTable() {
   }
   cellClick(table);
 }
-
 createTable();
-
 function changePlayer() {
   currentTurn = currentTurn === "X" ? "O" : "X";
-  clearInterval(timeInterval);
-  timeInterval = setInterval(rewindTimer, 1000);
-  timeout = setTimeout(timeOut, 10000);
-  seconds = 0;
-  turnCounter++;
-  rewindTimer();
 }
-
 function insertValue(tableCell, table) {
   if (tableCell.innerHTML === "" && currentTurn === "O") {
     tableCell.innerHTML = "X";
     tableCell.style.backgroundColor = "rgb(124, 252, 0)";
-    resetTimeout();
     changePlayer();
   }
   if (tableCell.innerHTML === "") {
     tableCell.innerHTML = "O";
     tableCell.style.backgroundColor = "rgb(250, 128, 114)";
-    resetTimeout();
     changePlayer();
-  }
-  if (turnCounter === 25) {
-    resetTable();
   }
   checkWinner();
 }
-
 function cellClick(table) {
   for (var i = 0; i < table.rows.length; i++) {
     for (var j = 0; j < table.rows[i].cells.length; j++) {
@@ -110,22 +63,6 @@ function cellClick(table) {
     }
   }
 }
-
-const winCombinations = [
-  [0, 1, 2, 3, 4],
-  [5, 6, 7, 8, 9],
-  [10, 11, 12, 13, 14],
-  [15, 16, 17, 18, 19],
-  [20, 21, 22, 23, 24],
-  [0, 5, 10, 15, 20],
-  [1, 6, 11, 16, 21],
-  [2, 7, 12, 17, 22],
-  [3, 8, 13, 18, 23],
-  [4, 9, 14, 19, 24],
-  [0, 6, 12, 18, 24],
-  [4, 8, 12, 16, 20]
-];
-
 function checkWinner() {
   let roundWin = false;
   let table = document.getElementById("board");
@@ -135,7 +72,6 @@ function checkWinner() {
     let c = table.rows[i].cells[2].innerHTML;
     let d = table.rows[i].cells[3].innerHTML;
     let e = table.rows[i].cells[4].innerHTML;
-
     if (a === "" || b === "" || c === "" || d === "" || e === "") {
       continue;
     }
@@ -150,7 +86,6 @@ function checkWinner() {
     let c = table.rows[2].cells[i].innerHTML;
     let d = table.rows[3].cells[i].innerHTML;
     let e = table.rows[4].cells[i].innerHTML;
-
     if (a === "" || b === "" || c === "" || d === "" || e === "") {
       continue;
     }
@@ -165,7 +100,6 @@ function checkWinner() {
     let c = table.rows[2].cells[2].innerHTML;
     let d = table.rows[3].cells[3].innerHTML;
     let e = table.rows[4].cells[4].innerHTML;
-
     if (a === "" || b === "" || c === "" || d === "" || e === "") {
       continue;
     }
@@ -180,7 +114,6 @@ function checkWinner() {
     let c = table.rows[2].cells[2].innerHTML;
     let d = table.rows[3].cells[1].innerHTML;
     let e = table.rows[4].cells[0].innerHTML;
-
     if (a === "" || b === "" || c === "" || d === "" || e === "") {
       continue;
     }
@@ -196,17 +129,11 @@ function checkWinner() {
     if (currentTurn === "O") {
       alert("Player 2 won!");
     }
-    gameActive = false;
-    resetTimeout();
     resetTable();
-    resetWidth();
-    turnCounter = 0;
     return;
   }
 }
-
 function resetTable() {
   document.getElementById("board").innerHTML = "";
-  turnCounter = 0;
   createTable();
 }
